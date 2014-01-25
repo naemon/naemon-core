197ca111c07473e1b207768470dbff9ddb78a796
Robin Sonefors <ozamosi@flukkost.nu>
2014-01-25 11:24:18 +0100

    Add autogen.sh script
    While autoreconf these days is mostly good enough for most projects to
    just use straight up, I became hardened in the bad, old days, when you
    needed pages of random shell scripts to help it put a configure script
    in place.
    
    Fearing for the return of those days, I'm preemptively applying the
    common workaround from those days: an autogen.sh script that invokes
    autoreconf and any other relevant tools with all the necessary options -
    whatever those turn out to be over time.
    
    Signed-off-by: Robin Sonefors <ozamosi@flukkost.nu>

---
+++ autogen.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+autoreconf -s -i
+# not enabling silent rules through configure.ac macros, as I'd like
+# non-interactive build outputs to be useful, despite interactive builds
+# looking pleasant
+./configure --enable-silent-rules
