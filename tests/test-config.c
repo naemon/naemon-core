22f1882b8d232c6a011721a754e299fbf2a78e97
Robin Sonefors <robin.sonefors@op5.com>
2014-02-05 12:07:42 +0100

    tests: Add test for recursive object configuration
    I was told naemon behaves unpredictably when an object inherits itself.
    I don't think it does, and I use this test to prove it.
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

---
+++ tests/test-config.c
@@ -0,0 +1,48 @@
+#include "naemon/configuration.h"
+#include "naemon/utils.h"
+
+#include <check.h>
+
+START_TEST(recursive)
+{
+	int res, hits;
+	host *h;
+	res = reset_variables();
+	ck_assert_int_eq(OK, res);
+	res = read_main_config_file("configs/recursive/naemon.cfg");
+	ck_assert_int_eq(OK, res);
+	res = read_all_object_data("configs/recursive/naemon.cfg");
+	ck_assert_int_eq(OK, res);
+	for (h = host_list, hits=0; h; h = h->next, hits++) {
+		if (!strcmp(h->name, "host1")) {
+			ck_assert_str_eq("from_template", h->alias);
+		} else if (!strcmp(h->name, "hosttemplate")) {
+			ck_assert_str_eq("from_template", h->alias);
+		} else {
+			ck_abort_msg("Found unexpected host: %s", h->name);
+		}
+	}
+	ck_assert_msg(hits == 2, "Expected 2 hosts, found %i", hits);
+}
+END_TEST
+
+Suite*
+config_suite(void)
+{
+	Suite *s = suite_create("Config");
+	TCase *parse = tcase_create("Parse configuration");
+	tcase_add_test(parse, recursive);
+	suite_add_tcase(s, parse);
+	return s;
+}
+
+int main(void)
+{
+	int number_failed = 0;
+	Suite *s = config_suite();
+	SRunner *sr = srunner_create(s);
+	srunner_run_all(sr, CK_NORMAL);
+	number_failed = srunner_ntests_failed(sr);
+	srunner_free(sr);
+	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
