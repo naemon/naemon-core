9e28c71b0cf98b2ad0a81f9356ce129ac47bb2d0
Andreas Ericsson <ae@op5.se>
2014-02-20 11:15:18 +0100

    autogen.sh: Accept and pass on command line arguments
    We'll want people to be able to build with non-default --prefix
    and things like that, so let's help users do just that.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- autogen.sh
+++ autogen.sh
@@ -4,4 +4,4 @@ autoreconf -s -i
 # not enabling silent rules through configure.ac macros, as I'd like
 # non-interactive build outputs to be useful, despite interactive builds
 # looking pleasant
-./configure --enable-silent-rules
+./configure --enable-silent-rules "$@"
