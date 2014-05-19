3cb9d78979fdfc67602db0250a1efadd77eccdc6
Andreas Ericsson <ae@op5.se>
2014-05-19 10:55:02 +0200

    pqueue.[ch]: Update license (and some comments)
    The author of libpqueue, Volkan Yazici, emailed me mentioning
    that Tom Callaway of RedHat had pointed out a licence
    incompatibility between libpqueue and the rest of the code,
    with a suggestion that I update the code accordingly.
    
    Reported-by: Volkan Yazici <volkan.yazici@gmail.com>
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- naemon/lib/pqueue.h
+++ naemon/lib/pqueue.h
@@ -1,19 +1,28 @@
 /*
- * Copyright 2010 Volkan Yazıcı <volkan.yazici@gmail.com>
- * Copyright 2006-2010 The Apache Software Foundation
+ * Copyright 2014 Volkan Yazıcı <volkan.yazici@gmail.com>
+ * All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
 #ifndef LIBNAEMON_pqueue_h__
 #define LIBNAEMON_pqueue_h__
 #include <stdio.h>
@@ -28,15 +37,16 @@ NAGIOS_BEGIN_DECL
  * This priority queue library was originally written by Volkan Yazici
  * <volkan.yazici@gmail.com>. It was lated adapted for Nagios by
  * Andreas Ericsson <ae@op5.se>. Changes compared to the original
- * version are pretty much limited to changing pqueue_pri_t to be
- * an unsigned long long instead of a double, since ULL comparisons
- * are 107 times faster on my 64-bit laptop.
+ * version are pretty much limited to adding doxygen comments, although
+ * once upon libpqueue had double instead of unsigned long long as the
+ * priority type, but has since changed their type to unsigned long
+ * long as well.
  *
  * @{
  */
 
 
-/** priority data type (used to be double, but ull is 107 times faster) */
+/** priority data type */
 typedef unsigned long long pqueue_pri_t;
 
 /** callback functions to get/set/compare the priority of an element */
