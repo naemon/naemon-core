768b6c19305b6122290341884695fcd4fb9329ed
Anton Lofgren <alofgren@op5.com>
2014-07-01 07:28:40 +0200

    utils: Add uninterrupted_write()
    This function lets us transparently handle EAGAIN/EINTR during writes
    by retrying, which we want to do any time they pop up. This also has the
    added benefit of silencing warnings about unchecked returns for write
    calls, in addition to actually checking the return and logging a message
    if something goes horribly wrong, which should make debugging easier in
    the future.
    
    Signed-off-by: Anton Lofgren <alofgren@op5.com>

--- naemon/query-handler.c
+++ naemon/query-handler.c
@@ -36,8 +36,7 @@ static int qh_echo(int sd, char *buf, unsigned int len)
 		                 "Query handler that simply echoes back what you send it.");
 		return 0;
 	}
-	(void)write(sd, buf, len);
-	return 0;
+	return uninterrupted_write(sd, buf, len);
 }
 
 static struct query_handler *qh_find_handler(const char *name)
