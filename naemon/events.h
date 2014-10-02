b9f224211be7ff72d56cd699ad8553ac4ad97cb4
Max Sikstrom <msikstrom@op5.com>
2014-10-02 15:35:47 +0200

    events.h: Reorder methods, remove methods no longer implemented
    Since not all mehtods are implemented, we should clean up those. Just a
    minor cleanup
    
    Signed-off-by: Max Sikstrom <msikstrom@op5.com>

--- naemon/events.h
+++ naemon/events.h
@@ -71,13 +71,20 @@ int init_event_queue(void); /* creates the queue nagios_squeue */
 timed_event *schedule_event(time_t run_time, void (*callback)(void *), void *args);
 
 timed_event *schedule_new_event(int, int, time_t, int, unsigned long, void *, int, void *, void *, int);	/* schedules a new timed event */
+
+/* Only used internally */
 void reschedule_event(squeue_t *sq, timed_event *event);   		/* reschedules an event */
+
+int handle_timed_event(timed_event *);		     		/* top level handler for timed events */
+void compensate_for_system_time_change(unsigned long, unsigned long);	/* attempts to compensate for a change in the system time */
+
+/* Lowlevel interface, deprecated */
 void add_event(squeue_t *sq, timed_event *event);     		/* adds an event to the execution queue */
 void remove_event(squeue_t *sq, timed_event *event);     		/* remove an event from the execution queue */
+
+/* Main function */
 int event_execution_loop(void);                      		/* main monitoring/event handler loop */
-int handle_timed_event(timed_event *);		     		/* top level handler for timed events */
-void adjust_check_scheduling(void);		        	/* auto-adjusts scheduling of host and service checks */
-void compensate_for_system_time_change(unsigned long, unsigned long);	/* attempts to compensate for a change in the system time */
+
 void adjust_timestamp_for_time_change(time_t, time_t, unsigned long, time_t *); /* adjusts a timestamp variable for a system time change */
 
 NAGIOS_END_DECL
