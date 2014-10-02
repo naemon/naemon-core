87eb44473fc10e8df517654cb45e2db50c1e6c06
Max Sikstrom <msikstrom@op5.com>
2014-10-02 16:39:34 +0200

    Let checks.c handle check scheduling intitalization
    Since the modules should know about themself, and not depend on other
    parts to initialize it's events, the check execution initialization
    should be handled in the check exeuction module, not in the event
    scheduling module.
    
    It lead to really nasty dependencies, and still does, but not as much.
    
    Change-Id: Ic1e7d8ac9655924115dc6690af3ba751b8e31131
    Signed-off-by: Max Sikstrom <msikstrom@op5.com>

--- src/naemon/naemon.c
+++ src/naemon/naemon.c
@@ -21,6 +21,8 @@
 #include "globals.h"
 #include "logging.h"
 #include "nm_alloc.h"
+#include "checks.h"
+
 #include <getopt.h>
 #include <string.h>
 
@@ -310,6 +312,12 @@ int main(int argc, char **argv)
 	 * are set.
 	 */
 	signal(SIGXFSZ, sighandler);
+
+
+	/*
+	 * Setup rand and srand. Don't bother with better resolution than second
+	 */
+	srand(time(NULL));
 
 	/*
 	 * let's go to town. We'll be noisy if we're verifying config
@@ -647,6 +655,10 @@ int main(int argc, char **argv)
 		/* initialize performance data */
 		initialize_performance_data(config_file);
 		timing_point("Performance data initialized\n");
+
+		/* initialize the check execution subsystem */
+		checks_init();
+		timing_point("Check execution scheduling initialized\n");
 
 		/* initialize the event timing loop */
 		init_timing_loop();
