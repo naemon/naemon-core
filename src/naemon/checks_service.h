bbd197992c7b651a3c5e24d3c9f29466032097c6
Max Sikstrom <msikstrom@op5.com>
2014-10-08 18:15:03 +0200

    checks: Dont expose more methods than nessecary
    We want to refactor the check module, so don't expose more methods than
    nessecary.
    
    Change-Id: I428cd1cf07a3f8fe0b6f387df5ce1fc5f5a229bc
    Signed-off-by: Max Sikstrom <msikstrom@op5.com>

--- src/naemon/checks_service.h
+++ src/naemon/checks_service.h
@@ -10,15 +10,10 @@
 NAGIOS_BEGIN_DECL
 
 void checks_init_services(void);
-
-void check_for_orphaned_services(void);				/* checks for orphaned services */
-int check_service_dependencies(service *, int);          	/* checks service dependencies */
-int is_service_result_fresh(service *, time_t, int);            /* determines if a service's check results are fresh */
-int check_service_check_viability(service *, int, int *, time_t *);
-int run_scheduled_service_check(service *, int, double);
-int run_async_service_check(service *, int, double, int, int, int *, time_t *);
+void check_for_orphaned_services(void);	/* checks for orphaned services */
+int check_service_dependencies(service *, int); /* checks service dependencies */
 int handle_async_service_check_result(service *, check_result *);
-void schedule_service_check(service *, time_t, int);	/* schedules an immediate or delayed service check */
+void schedule_service_check(service *, time_t, int); /* schedules an immediate or delayed service check */
 
 NAGIOS_END_DECL
 
