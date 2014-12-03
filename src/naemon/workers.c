3920c546021a6b9b3d3c3851c37d4afe25ab5be3
Robin Sonefors <robin.sonefors@op5.com>
2014-12-03 09:30:17 +0100

    workers: Remove actively lying comment
    Workers are *not* retained during restarts.
    
    Change-Id: I4deae5d5b22e06ca4095a18b00f00f4ebf9d535f
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- src/naemon/workers.c
+++ src/naemon/workers.c
@@ -248,7 +248,6 @@ static int wproc_destroy(struct wproc_worker *wp, int flags)
 
 	self = getpid();
 
-	/* master retains workers through restarts */
 	if (self == nagios_pid && !force)
 		return 0;
 
