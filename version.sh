b2cac147e56b852865ffbf5d78215bf17f7b9086
Sven Nierlein <Sven.Nierlein@consol.de>
2015-01-08 14:09:07 +0100

    release v0.9.1
    Signed-off-by: Sven Nierlein <Sven.Nierlein@consol.de>

--- version.sh
+++ version.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-if test -d .git; then
+if test -e .git; then
 	git describe --always --tags --dirty | \
 		sed -e 's/^v//' -e 's/-[0-9]*-g/-g/' | tr -d '\n'
 	exit 0
 fi
 
-VERSION=0.9.0
+VERSION=0.9.1
 echo -n "${VERSION}-source"
