c74cf3dc23d4e0848ab071589211f0359447a919
Max Sikstrom <max.sikstrom@op5.com>
2015-05-04 11:06:43 +0200

    Let environment set test output verbosity
    Setting the verbosity level of checks in the test itself makes it hard
    to debug test cases. When developing tests, verbose output is nice, but
    almost everywhere else, normal (only faild tests) is nice to get output
    from.
    
    So just use enviornment to define test verbosity. If not defined in
    enviorment, CK_NORMAL is the default, thus no change if not explicitly
    asking for it during test-execution.
    
    Change-Id: I4258f10c93ec6c46be04d0859c70fe00883d4454
    Signed-off-by: Max Sikstrom <max.sikstrom@op5.com>

--- tests/test-event-heap.c
+++ tests/test-event-heap.c
@@ -213,7 +213,7 @@ int main(void)
 	int number_failed = 0;
 	Suite *s = event_heap_suite();
 	SRunner *sr = srunner_create(s);
-	srunner_run_all(sr, CK_NORMAL);
+	srunner_run_all(sr, CK_ENV);
 	number_failed = srunner_ntests_failed(sr);
 	srunner_free(sr);
 	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
--- tests/test-kv-command.c
+++ tests/test-kv-command.c
@@ -132,7 +132,7 @@ int main(void) {
 	int number_failed = 0;
 	Suite *s = kv_command_suite();
 	SRunner *sr = srunner_create(s);
-	srunner_run_all(sr, CK_NORMAL);
+	srunner_run_all(sr, CK_ENV);
 	number_failed = srunner_ntests_failed(sr);
 	srunner_free(sr);
 	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
