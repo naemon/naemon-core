01a47df0dd6ababcaa931d07fd83743d135d4994
Julien Thomas <jthomas@pom-monitoring.com>
2016-01-26 17:24:01 +0100

    xrddefault: Fix custom variables persistence accros restarts
    This fix is not Naemon specific, it is also applicable to Nagios.
    
    Edit Sven Nierlein <Sven.Nierlein@consol.de>:
    
    Naemon writes changed custom variables prefixed with an option
    value into the retention.dat like
    
    _VARNAME=1;value
    
    On startup it then checks if the lenght of 1;value is more
    than 3 which in fact skips changed custom variables if they
    only contain a single character or nothing at all.

--- src/naemon/xrddefault.c
+++ src/naemon/xrddefault.c
@@ -1181,7 +1181,7 @@ int xrddefault_read_state_information(void)
 
 								for (temp_customvariablesmember = temp_host->custom_variables; temp_customvariablesmember != NULL; temp_customvariablesmember = temp_customvariablesmember->next) {
 									if (!strcmp(customvarname, temp_customvariablesmember->variable_name)) {
-										if ((x = atoi(val)) > 0 && strlen(val) > 3) {
+										if ((x = atoi(val)) > 0 && strlen(val) >= 2) {
 											nm_free(temp_customvariablesmember->variable_value);
 											temp_customvariablesmember->variable_value = nm_strdup(val + 2);
 											temp_customvariablesmember->has_been_modified = (x > 0) ? TRUE : FALSE;
@@ -1443,7 +1443,7 @@ int xrddefault_read_state_information(void)
 								customvarname = nm_strdup(var + 1);
 								for (temp_customvariablesmember = temp_service->custom_variables; temp_customvariablesmember != NULL; temp_customvariablesmember = temp_customvariablesmember->next) {
 									if (!strcmp(customvarname, temp_customvariablesmember->variable_name)) {
-										if ((x = atoi(val)) > 0 && strlen(val) > 3) {
+										if ((x = atoi(val)) > 0 && strlen(val) >= 2) {
 											nm_free(temp_customvariablesmember->variable_value);
 											temp_customvariablesmember->variable_value = nm_strdup(val + 2);
 											temp_customvariablesmember->has_been_modified = (x > 0) ? TRUE : FALSE;
@@ -1551,7 +1551,7 @@ int xrddefault_read_state_information(void)
 								customvarname = nm_strdup(var + 1);
 								for (temp_customvariablesmember = temp_contact->custom_variables; temp_customvariablesmember != NULL; temp_customvariablesmember = temp_customvariablesmember->next) {
 									if (!strcmp(customvarname, temp_customvariablesmember->variable_name)) {
-										if ((x = atoi(val)) > 0 && strlen(val) > 3) {
+										if ((x = atoi(val)) > 0 && strlen(val) >= 2) {
 											nm_free(temp_customvariablesmember->variable_value);
 											temp_customvariablesmember->variable_value = nm_strdup(val + 2);
 											temp_customvariablesmember->has_been_modified = (x > 0) ? TRUE : FALSE;
