b9b9147d54427ad334b32a06c45cfbb60b8ab7ab
Sven Nierlein <Sven.Nierlein@consol.de>
2017-01-11 17:45:34 +0100

    add error when started as root
    instead of creating (log)files as root and chown them later we simply
    should not be started as root at all. This prevents the root exploit
    then systemV init scripts start naemon as root user.
    
    Signed-off-by: Sven Nierlein <Sven.Nierlein@consol.de>

--- src/naemon/logging.c
+++ src/naemon/logging.c
@@ -175,24 +175,6 @@ static void write_to_all_logs_with_timestamp(char *buffer, unsigned long data_ty
 	write_to_log(buffer, data_type, timestamp);
 }
 
-
-int fix_log_file_owner(uid_t uid, gid_t gid)
-{
-	int r1 = 0, r2 = 0;
-
-	if (!(log_fp = open_log_file()))
-		return -1;
-	r1 = fchown(fileno(log_fp), uid, gid);
-
-	if (open_debug_log() != OK)
-		return -1;
-	if (debug_file_fp)
-		r2 = fchown(fileno(debug_file_fp), uid, gid);
-
-	/* return 0 if both are 0 and otherwise < 0 */
-	return r1 < r2 ? r1 : r2;
-}
-
 int close_log_file(void)
 {
 	if (!log_fp)
