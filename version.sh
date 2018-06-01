eda6300f8ccacf4150321e137eb1dd55ed86413c
Sven Nierlein <sven@nierlein.de>
2018-06-01 14:13:31 +0200

    do not append date to version on exact tags

--- version.sh
+++ version.sh
@@ -3,8 +3,11 @@
 VERSION=1.0.6
 if test -e .git; then
     if hash git 2>/dev/null; then
-        VERSION=$(git describe --always --tags --dirty | \
-            sed -e 's/^v//' -e 's/-[0-9]*-g/-g/' | tr -d '\n' | tr '-' '.').$(date +%Y%m%d)
+        VERSION=$(git describe --tag --exact-match 2>/dev/null | sed -e 's/^v//')
+        if [ "x$VERSION" = "x" ]; then
+            VERSION=$(git describe --always --tags --dirty | \
+                sed -e 's/^v//' -e 's/-[0-9]*-g/-g/' | tr -d '\n' | tr '-' '.').$(date +%Y%m%d)
+        fi
     fi
 fi
 
