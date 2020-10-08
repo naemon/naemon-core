43223512929efc4d6fd9898bbede30e7f3f0888a
Aksel Sjögren <asjogren@itrsgroup.com>
2020-10-08 14:51:09 +0200

    Fix indent-all.sh script
    * Fix source paths in indent-all.sh
    
      The indent-all.sh script is supposed to re-format the whole tree. But
      source code were moved around in 7344116806274b without updating
      indent-all.sh.
    
    * Add --check option to indent-all.sh
    
      Add a --check option to indent-all.sh that fails if there are astyle
      inconsistencies. This should run in CI whenever a modern astyle
      version is available there.
    
    Signed-off-by: Aksel Sjögren <asjogren@itrsgroup.com>

--- indent-all.sh
+++ indent-all.sh
@@ -1,6 +1,46 @@
 #!/bin/sh
 
-for f in `find ./naemon ./t-tap -type f -name "*.[c]"`; do
-	./indent.sh $f
-done
+# Usage: indent-all.sh [--check]
 
+set -e
+
+ARTISTIC_STYLE_OPTIONS=/dev/null
+export ARTISTIC_STYLE_OPTIONS
+tmpfile=''
+
+cleanup() {
+	test "$tmpfile" != '' && rm -f "$tmpfile"
+}
+trap cleanup EXIT
+
+astyle_options="--style=linux \
+	--indent=tab \
+	--unpad-paren \
+	--pad-oper \
+	--pad-header \
+	--lineend=linux \
+	--align-pointer=name \
+	--keep-one-line-statements \
+	--preserve-date \
+	--formatted \
+	--recursive \
+	--errors-to-stdout"
+
+astyle_paths='lib/\*.c src/\*.c t-tap/\*.c tests/\*.c'
+
+if ! hash astyle 2> /dev/null; then
+	echo "ERROR: 'astyle' is not installed on the system, cannot continue indenting..." >&2
+	exit 1
+fi
+
+if [ "${1:-}" = "--check" ]; then
+	tmpfile=$(mktemp)
+	astyle $astyle_options --dry-run $astyle_paths | tee "$tmpfile"
+	if grep -q Formatted "$tmpfile"; then
+		echo "ERROR: astyle regressions found. Run '$0' to fix." >&2
+		exit 1
+	fi
+	exit 0
+fi
+
+astyle $astyle_options --suffix=.pre-indent $astyle_paths
